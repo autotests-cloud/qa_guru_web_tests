020369437c25874cc27c52881168f79dd4b997fe
svasenkov <aanher@gmail.com>
2020-09-21 21:52:01 +0300

    6. Добавляем вложения в Allure Reports

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,13 +1,19 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import drivers.CustomWebDriver;
 import helpers.PropertiesHelper;
+import io.qameta.allure.Step;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.AttachmentsHelper.*;
+import static org.openqa.selenium.logging.LogType.BROWSER;
 
 
 public class TestBase {
@@ -22,6 +28,20 @@ public class TestBase {
 
         Configuration.browser = CustomWebDriver.class.getName();
         Configuration.baseUrl = config.webUrl();
-
     }
+
+    @AfterEach
+    @Step("Attachments")
+    public void afterEach(){
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Browser console logs", getBrowserConsoleLogs());
+
+        closeWebDriver();
+    }
+
+    public static String getBrowserConsoleLogs() {
+        return String.join("\n", Selenide.getWebDriverLogs(BROWSER));
+    }
+
 }
