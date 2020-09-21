977175d70fd37319db76110f1765b4718bfb7da0
svasenkov <aanher@gmail.com>
2020-09-21 20:45:02 +0300

    3. Создаем джобу в Jenkins, запускаем с локальным браузером, обновляем код (чтение remote.properties)

--- src/test/java/helpers/PropertiesHelper.java
+++ src/test/java/helpers/PropertiesHelper.java
@@ -2,7 +2,7 @@ package helpers;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources({"classpath:local.properties"})
+@Config.Sources({"classpath:local.properties", "classpath:remote.properties"})
 public interface PropertiesHelper extends Config {
 
     @Key("web.url")
