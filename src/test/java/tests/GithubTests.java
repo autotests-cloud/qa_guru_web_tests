45e9a7d32ae09c5566cba5118f41dc8362b6be3f
svasenkov <aanher@gmail.com>
2020-09-21 21:45:48 +0300

    5. Подключаем Allure Reports

--- src/test/java/tests/GithubTests.java
+++ src/test/java/tests/GithubTests.java
@@ -1,20 +1,31 @@
 package tests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 public class GithubTests extends TestBase {
 
     @Test
+    @DisplayName("Login with unverified email")
     void loginWithUnverifiedEmailTest() {
-        open("/login");
+        step("Go to login page", () -> {
+            open("/login");
+            $(".auth-form-header").shouldBe(visible);
+        });
 
-        $("#login_field").setValue(config.unverifiedUserName());
-        $("#password").setValue(config.unverifiedUserPassword()).pressEnter();
+        step("Fill the authorization form", () -> {
+            $("#login_field").setValue(config.unverifiedUserName());
+            $("#password").setValue(config.unverifiedUserPassword()).pressEnter();
+        });
 
-        $(".flash-error").shouldHave(text("Your account has been flagged."));
+        step("Verify flagged authorization", () -> {
+            $(".flash-error").shouldHave(text("Your account has been flagged."));
+        });
     }
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,8 +3,11 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import drivers.CustomWebDriver;
 import helpers.PropertiesHelper;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 
 
 public class TestBase {
@@ -15,6 +18,7 @@ public class TestBase {
     @BeforeAll
     public static void beforeAll() {
         config = ConfigFactory.newInstance().create(PropertiesHelper.class, System.getProperties());
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
 
         Configuration.browser = CustomWebDriver.class.getName();
         Configuration.baseUrl = config.webUrl();
