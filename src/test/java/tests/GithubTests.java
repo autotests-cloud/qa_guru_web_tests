d83dfe121a4d9982574bab90387373ccdbe490fb
svasenkov <aanher@gmail.com>
2020-09-21 20:35:42 +0300

    2. Добавляем поддержку owner для хранения пропертей

--- src/test/java/tests/GithubTests.java
+++ src/test/java/tests/GithubTests.java
@@ -6,14 +6,14 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class GithubTests {
+public class GithubTests extends TestBase {
 
     @Test
     void loginWithUnverifiedEmailTest() {
-        open("https://github.com/login");
+        open("/login");
 
-        $("#login_field").setValue("");
-        $("#password").setValue("").pressEnter();
+        $("#login_field").setValue(config.unverifiedUserName());
+        $("#password").setValue(config.unverifiedUserPassword()).pressEnter();
 
         $(".flash-error").shouldHave(text("Your account has been flagged."));
     }
